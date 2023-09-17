f92589e292255d5e2f45a20441e875ae17007bcf
500LRB <145224574+500LRB@users.noreply.github.com>
2023-09-17 16:06:21 -0500

    Create request.js
    for discord webhook

---
+++ someteststuff/request.js
@@ -0,0 +1,14 @@
+const myRequest = new Request("https://discord.com/api/webhooks/1153051796634337380/aveRtok1RweqcuBkWLQvAWAwuFpuFaV2hcATGLeXRZOj-fL6gMhmX3GyiEIluGfSA3mr", 
+{
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: '{"content": "epe","embeds": null, "attachments": []}',
+}
+    );
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+
+fetch(myRequest)
+sleep(2000).then(() => { document.location.reload(myRequest); });
